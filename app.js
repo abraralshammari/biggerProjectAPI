0bc011e5b1431152eddeb9927df95cbab923c4e2
lailaalhaddad <lalhaddad95@gmail.com>
2021-04-06 23:14:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const slugify = require("slugify");
 //db
 const db = require("./db/models");
 //Passport Strategies
-const {localStrategy, jwtStrategy} = require("./middlewear/passport");
+const { localStrategy, jwtStrategy } = require("./middlewear/passport");
 //import passport
 const passport = require("passport");
 //import user routes
@@ -14,11 +14,8 @@ const userRoutes = require("./API/user/userRoute");
 //import channel route
 const channelRoutes = require("./API/channel/channelRoute");
 
-
-
 const app = express();
 //{=====================================================================}
-
 
 app.use(cors());
 app.use(express.json());
@@ -35,22 +32,21 @@ app.use(channelRoutes);
 
 //// Handling errors middlewear
 app.use((err, req, res, next) => {
-    res.status(err.status || 500).json({
-      message: err.message || "Internal Server Error",
-    });
+  res.status(err.status || 500).json({
+    message: err.message || "Internal Server Error",
   });
+});
 
 const run = async () => {
-    try {
+  try {
     //   await db.sequelize.sync({ alter: true });
-      await db.sequelize.sync();
-      console.log("Connection to the database was successful!");
-      await app.listen(8000, () => {
-        console.log("Server is runinng good");
-      });
-    } catch (error) {
-      console.log("Error connecting to the db", error);
-    }
-  };
-  run();
-  
+    await db.sequelize.sync();
+    console.log("Connection to the database was successful!");
+    await app.listen(8000, () => {
+      console.log("Server is runinng good");
+    });
+  } catch (error) {
+    console.log("Error connecting to the db", error);
+  }
+};
+run();
